3f5de40ba57d7b117c79589a50efd73abfb8aad7
Ben <bngarren@gmail.com>
2020-07-30 03:29:54 +0000

    fixed homepage

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,18 +1,29 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
 import * as ROUTES from "../../constants/routes";
 
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    paddingTop: "30px",
+  },
+});
+
 const HomePage = () => {
+  const classes = useStyles();
+
   // This is just hard coded link for development
   const noteLink = `${ROUTES.NOTE}/cbNW3nAieisZyUuXF8pN`;
 
   return (
-    <div>
-      This is the home page
-      <br />
-      <Link to={noteLink}>Note</Link>
-    </div>
+    <Container className={classes.root}>
+      <Typography variant="h4">Welcome</Typography>
+    </Container>
   );
 };
 
