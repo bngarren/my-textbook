b846ae5760aa00f6f15dfd0a1fcc75dc0f46b02b
Ben <bngarren@gmail.com>
2020-08-08 22:16:13 +0000

    - added LastSaved time to EditNoteWorkspace - commenting to useNoteAndCards.js

--- src/components/ViewNote/Workspace/EditNoteWorkspace/index.js
+++ src/components/ViewNote/Workspace/EditNoteWorkspace/index.js
@@ -4,6 +4,7 @@ import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
+import CloudDoneIcon from "@material-ui/icons/CloudDone";
 
 import {
   useNoteAndCards,
@@ -14,15 +15,36 @@ const useStyles = makeStyles({
   root: {
     minHeight: "25px",
   },
+  syncStatus: {
+    display: "flex",
+  },
 });
 
 const EditNoteWorkspace = () => {
   const classes = useStyles();
   const [noteAndCardsState, dispatchNoteAndCards] = useNoteAndCards();
 
-  const getLastSavedTime = () => {
-    const date = noteAndCardsState.lastSaved.toDate();
-    return `${date.toLocaleString()}`;
+  const getTimeSinceLastSaved = () => {
+    const lastSaved = noteAndCardsState.lastSaved.toDate();
+    const now = new Date();
+    const diff = new Date(now - lastSaved);
+
+    const secs = diff / 1000.0;
+    const mins = secs / 60.0;
+    const hrs = mins / 60.0;
+    const days = hrs / 24.0;
+
+    if (days >= 1) {
+      return lastSaved.toLocaleDateString();
+    } else if (hrs >= 1) {
+      const suffix = hrs < 2 ? "" : "s";
+      return `${Math.floor(hrs)} hour${suffix} ago`;
+    } else if (mins >= 1) {
+      const suffix = mins < 2 ? "" : "s";
+      return `${Math.floor(mins)} minute${suffix} ago`;
+    } else {
+      return `1 minute ago`;
+    }
   };
 
   const handleOnSave = () => {
@@ -34,9 +56,11 @@ const EditNoteWorkspace = () => {
 
   return (
     <Container className={classes.root}>
-      <Typography variant="body1">
-        <>Last saved {getLastSavedTime()}</>
-      </Typography>
+      <div className={classes.syncStatus}>
+        {noteAndCardsState.noteIsSynced && <CloudDoneIcon />}
+        <Typography>Last saved: {getTimeSinceLastSaved()}</Typography>
+      </div>
+
       {!noteAndCardsState.noteIsSynced ? (
         <>
           <Button onClick={handleOnSave}>Save</Button>
--- src/hooks/useNoteAndCards.js
+++ src/hooks/useNoteAndCards.js
@@ -1,12 +1,22 @@
-import React, {
-  createContext,
-  useContext,
-  useCallback,
-  useReducer,
-} from "react";
+import React, { createContext, useContext, useReducer } from "react";
 
 import { addCard, saveNote } from "../components/Firebase";
 
+/*
+ * This is a 'custom hook' but is largely just a custom Context. The NoteAndCardsContextProvider
+ * is implemented in the ViewNote>index.js page and sits atop the component tree that includes
+ * Toolbar, Workspace, and MarkdownEditor. The intent is to provide common props to all these
+ * components without having to pass everything down via prop drilling.
+ *
+ * The NoteAndCardsContextProvider function is a wrapper for the Context.Provider, but more importantly
+ * helps to manage the state of the context's data by way of a useReducer pattern. In this way,
+ * when the context is consumed in a child component via useNoteAndCards() hook (which itself calls
+ * useContext), that component gets returned the state and a dispatch function which can be
+ * used to update state from a child component.
+ *
+ */
+
+/* An enum that formalizes the different action types that can be called with dispatch*/
 export const NOTE_AND_CARDS_ACTION = Object.freeze({
   ADD_CARD: "addCard",
   UPDATE_NOTE_IS_EDITABLE: "updateNoteIsEditable",
@@ -14,6 +24,10 @@ export const NOTE_AND_CARDS_ACTION = Object.freeze({
   SAVE_NOTE: "saveNote",
 });
 
+/* These are the data types that the context will carry, initialized to null to start*/
+/* Idk if this is kosher, but I even stored a callback function here so that when a certain
+dispatch action is performed, the NoteAndCardsContextProvider has access to a certain component
+via this callback*/
 const initialNullState = {
   setId: null,
   userId: null,
@@ -26,12 +40,15 @@ const initialNullState = {
   mdParser: null,
 };
 
+// create the context
 const NoteAndCardsContext = createContext(initialNullState);
 
+// this is the 'custom hook', but this hook really only gives us the context
 export const useNoteAndCards = () => {
   return useContext(NoteAndCardsContext);
 };
 
+// this is the meat and potatoes of this module
 export const NoteAndCardsContextProvider = ({
   children,
   initialState = { ...initialNullState },
@@ -45,6 +62,7 @@ export const NoteAndCardsContextProvider = ({
   );
 };
 
+// the reducer function implements the action types
 const Reducer = (state, action) => {
   switch (action.type) {
     case NOTE_AND_CARDS_ACTION.ADD_CARD:
